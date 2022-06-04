0a2c54d86a9596eec893f0ac42cc6de1deaf2280
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-06-04 13:15:21 +0530

    Redux bug here

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { FC } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { InterfaceReduxState, setCount } from "./stateManager/mainSlice"
 
 
-function App() {
+const App: FC = () => {
+
+  const count = useSelector((state: InterfaceReduxState)=> state.count)
+  const dispatch = useDispatch()
+  console.log(count)
+
   return (
     <>
-    Yo
+      <h1>{count}</h1>
+      <button>Inc</button>
+      <button>Dec</button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/stateManager/mainSlice.ts
+++ src/stateManager/mainSlice.ts
@@ -1,10 +1,15 @@
 import { CaseReducer, CaseReducerWithPrepare, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+//types
+export interface InterfaceReduxState {
+    count: number
+}
+
 export const mainSlice = createSlice({
     name: 'mainSlice',
     initialState: {
         count: 0
-    },
+    } as InterfaceReduxState,
     reducers: {
         setCount: (state, action)=> {
             state.count += 1
